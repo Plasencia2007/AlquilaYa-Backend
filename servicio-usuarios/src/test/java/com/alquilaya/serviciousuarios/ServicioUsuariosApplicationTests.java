b1311315aa51e8ea6e363b07fce9e6f572d2d2ac
Jhons <jhonsonsplasencia25@gmail.com>
2026-04-14 14:16:32 -0500

    Estructura inicial: Config, Discovery, Gateway y Servicio-Usuarios

---
+++ servicio-usuarios/src/test/java/com/alquilaya/serviciousuarios/ServicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquilaya.serviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
